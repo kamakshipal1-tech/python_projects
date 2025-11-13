71b0c435a1db7dc7cf5594c19112664a0e2aeb8c
Kamakshi Pal <kamakshipal1@gmail.com>
2025-11-13 23:29:17 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+price=10
+price=20
+print(price)
+
+
+birthyear= input('birth year:')
+age=2025 - int(birthyear)
+print(age)
+print(type(age))
+weight=input('weight in pounds :')
+weightkg=int(weight) *0.45
+print(weightkg)
